2cc6df74ec9a39ee97d6b2f2f822241db32a97cc
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2025-04-27 23:03:39 -0400

    Update iframe map

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
         <footer className="container-fluid p-0 pr-0">
     <div className="row mr-0 ml-0">
       <div className="col-md-6 pr-0 pl-0 map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13035.46901327725!2d-80.85396551628644!3d35.234674411422155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8856a03af474f38f%3A0xf8301daadf5f7670!2sFourth+Ward%2C+Charlotte%2C+NC%2C+USA!5e0!3m2!1sen!2sin!4v1551001218548"  style={{border:0}} allowfullscreen></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13035.46901327725!2d-80.85396551628644!3d35.234674411422155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8856a03af474f38f%3A0xf8301daadf5f7670!2sFourth+Ward%2C+Charlotte%2C+NC%2C+USA!5e0!3m2!1sen!2sin!4v1551001218548" title="Map"  style={{border:0}} allowfullscreen></iframe>
       </div>
       <div className="col-md-6 content-us">
         <div className="contact-form" id="contact">
