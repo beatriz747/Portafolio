d3c734f8b9ac7e690313f1be91102a8b18d91557
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2025-05-08 16:46:29 -0400

    ajuste secciones

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 
 import Activity from './sections/Activity';
 import Footer from './sections/Footer';
-import HomeBanner from './sections/HomeBanner';
 import InfoCard from './sections/InfoCard';
 import Navbar from './sections/Navbar';
 import Portfolio from './sections/Portfolio';
@@ -46,7 +45,6 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* <HomeBanner /> */}
       <InfoCard profile={profileData} />
       <Features features={profileData.features} />
       <Brands />
