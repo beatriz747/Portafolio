78063a64526d620d2b8dadde81b8cb508550546f
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2025-05-20 07:36:20 -0400

    Trabajos Recientes

--- src/sections/ProfileBio.js
+++ src/sections/ProfileBio.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import * as utils from '../utils';
 import './ProfileBio.css';
 
@@ -7,7 +6,6 @@ function ProfileBio({ profile }) {
     <div className="container-fluid fh5co-recent-work">
       <Background />
       <ContactCard profile={profile} />
-      <GalleryCards gallery={profile.gallery} />
     </div>
   );
 }
@@ -61,82 +59,7 @@ function ContactCard({ profile }) {
   );
 }
 
-function GalleryCards({ gallery }) {
 
-  useEffect(() => {
-    const UIHelper = global['UIHelper'];
-    UIHelper.initCarousel('#gallery-carousel', {
-      nav: false,
-      responsive:{
-        0:{
-          items:1,
-          nav:false,
-        },
-        600:{
-          items:1,
-          nav:false
-        },
-        1000:{
-          items:3,
-          nav:false,
-          loop:false
-        }
-      }
-    });
-  }, []);
-  
-  return (
-      <div className="container recent" id="recientes">
-      <div className="row">
-        <h2>{ gallery.title }</h2>
-        <div id="gallery-carousel" className="owl-carousel owl-carousel2 owl-theme">
-          { gallery.images.map((item, index) => (
-            <div key={"gallery-image-"+index}>
-              <div className="card">
-                {item.type === "image" && <GalleryImage item={item} />}
-                {item.type === "video" && <GalleryVideo item={item} />}
-              </div>
-            </div>
-          )) }
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function GalleryImage({item}) {
-  return (
-    <>
-      <img className="card-img" src={utils.assetUrl(item.url)} alt="" />
-      <div className="card-img-overlay">
-        <a href="/">
-          <img src={utils.assetUrl('img/heart.png')} className="heart" alt="heart icon" />
-        </a>
-        <div className="bottom-text">
-          <h5 className="card-title">{ item.title }</h5>
-          <p className="card-text">{ item.text }</p>
-        </div>
-      </div>
-    </>
-  );
-}
-
-function GalleryVideo({item}) {
-  return (
-    <>
-      <div className="card-video-overlay">
-        <div className="bottom-text">
-          <h5 className="card-title">{ item.title }</h5>
-          <p className="card-text">{ item.text }</p>
-        </div>
-      </div>
-      <video width="100%" height="500" controls>
-        <source src={utils.assetUrl(item.url)} type="video/mp4" />
-            Your browser does not support the video tag.
-      </video>
-    </>
-  );
-}
 
 function Background() {
   return (
---
+++ src/sections/RecentWork.js
@@ -0,0 +1,100 @@
+import { useEffect, useState } from 'react';
+import * as utils from '../utils';
+
+function RecentWork({ gallery }) {
+    return (
+        <div className="container-fluid fh5co-recent-work">
+            <VideoGallery gallery={gallery} />
+        </div>
+    );
+}
+
+function VideoGallery({ gallery }) {
+
+  useEffect(() => {
+    const UIHelper = global['UIHelper'];
+    UIHelper.initCarousel('#gallery-carousel', {
+      nav: false,
+      responsive:{
+        0:{
+          items:3,
+          nav:false,
+        },
+        600:{
+          items:4,
+          nav:false
+        },
+        1000:{
+          items:6,
+          nav:false,
+          loop:false
+        }
+      }
+    });
+  }, []);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentVideo = gallery.items[currentIndex];
+
+  
+  return (
+      <div className="container-fluid fh5co-recent" id="recientes">
+        <div className="container">
+            <h2>{ gallery.title }</h2>
+            <div className="row">
+                <div className="col-12"></div>
+                <div className="videoContainer">
+                    <VideoItem item={currentVideo} />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <div id="gallery-carousel" className="owl-carousel owl-carousel2 owl-theme">
+                    { gallery.items.map((item, index) => (
+                        <div key={"gallery-image-"+index} className="card">
+                            <img src={utils.assetUrl(item.thumbnail)} className="img-thumbnail" alt={"Thumb " + item.title} />
+                        </div>
+                    )) }
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+function GalleryImage({item}) {
+  return (
+    <>
+      <img className="card-img" src={utils.assetUrl(item.url)} alt="" />
+      <div className="card-img-overlay">
+        <a href="/">
+          <img src={utils.assetUrl('img/heart.png')} className="heart" alt="heart icon" />
+        </a>
+        <div className="bottom-text">
+          <h5 className="card-title">{ item.title }</h5>
+          <p className="card-text">{ item.text }</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function VideoItem({item}) {
+  return (
+    <>
+      <div className="card-video-overlay">
+        <div className="bottom-text">
+          <h5 className="card-title">{ item.title }</h5>
+          <p className="card-text">{ item.caption }</p>
+        </div>
+      </div>
+      <video width="100%" height="500" controls>
+        <source src={utils.assetUrl(item.url)} type="video/mp4" />
+            Your browser does not support the video tag.
+      </video>
+    </>
+  );
+}
+
+export default RecentWork;
