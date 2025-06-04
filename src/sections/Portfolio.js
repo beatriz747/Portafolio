7002dbd06146d6969761ddb0d0e75758e6314432
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2025-06-04 06:40:09 -0400

    portafolio: video destacado

--- src/sections/Portfolio.js
+++ src/sections/Portfolio.js
@@ -1,32 +1,43 @@
 import * as utils from '../utils';
 import './Portfolio.css';
 
+
 function Portfolio({ portfolio }) {
-    return (
-      <div className="container-fluid fh5co-portfolio section-bg-accent1" id="portafolio">
-        <div className="container">
-          <h2 className="h2 fw-bold">{ portfolio.title }</h2>
-          <p className="lead text-muted">{ portfolio.description }</p>
-          <div className="row">
-            { portfolio.images.map((image, index) => (
-              <div key={'portfolio-img-'+index} className="col-6 col-md-4 col-lg-3 gallery-item">
-                <img src={utils.assetUrl(image.url)} alt={image.title} class="img-fluid" />
-                <div className="overlay">
-                  <h5>{image.title}</h5>
-                </div>
-              </div>
-            ))}
+
+  return (
+    <div className="container-fluid fh5co-portfolio section-bg-accent1" id="portafolio">
+      <div className="container">
+        <div className="row justify-content-center mb-5">
+          <div className="col-12 col-lg-8 text-center">
+            <h2 className="h2 fw-bold">{ portfolio.title }</h2>
+            <p className="lead text-muted">{ portfolio.description }</p>
           </div>
-          <div className="row my-5">
-            <div className="col-12 text-center">
-              <h4>Muchos hacen lo mismo. Tú vas a destacar.</h4>
-              <br />
-              <h4>¿Trabajamos juntos?</h4>
+        </div>
+        <div className="row">
+          { portfolio.images.map((image, index) => (
+            <div key={'portfolio-img-'+index} className="col-6 col-md-4 gallery-item">
+              <img src={utils.assetUrl(image.url)} alt={image.title} className="img-fluid" />
             </div>
+          ))}
+        </div>
+        <div className="row my-5">
+          <div className="col-12 col-md-6 col-lg-4 mx-auto text-center">
+            <video id="featuredVideo" className="w-100" controls autoPlay>
+              <source src={utils.assetUrl(portfolio.featuredVideo.source)} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        </div>
+        <div className="row my-5">
+          <div className="col-12 text-center">
+            <h3>Muchos hacen lo mismo.</h3>
+            <h3 className="text-primary mb-4">Tú vas a destacar.</h3>
+            <p><a href={portfolio.cta} target="_blank" rel="noopener noreferrer" className="btn btn-dark btn-lg">¿Trabajamos juntos?</a></p>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Portfolio;
